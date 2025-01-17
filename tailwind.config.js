089bd457e4be0c7c800504232a8015c31d45bff6
elengaomerfils <elengaomerfils@gmail.com>
2025-01-17 23:45:57 +0000

    v3

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js}", // Assurez-vous que vos fichiers HTML/JS sont inclus ici
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'blue-primary': '#5C6CF2',
+          'orange': '#F2994A',
+          'violet-dark': '#200C38',
+          'yellow': '#F2C94C',
+          'gray-light': '#F2F2F2',
+          'black': '#000000',
+        },
+      },
+    },
+    plugins: [],
+  }
